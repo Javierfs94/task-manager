17ec302726fc907cc3fb1db41716b80f6634a5e8
Javierfs <javifs94@gmail.com>
2025-06-10 13:30:52 +0200

    i18n

---
+++ lang/en/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'app_name' => 'Task manager',
+    'task_list' => 'Task List',
+    'create_task' => 'Create Task',
+    'edit_task' => 'Edit Task',
+    'task_title' => 'Title',
+    'task_description' => 'Description',
+    'task_due_date' => 'Due Date',
+    'task_completed' => 'Completed',
+    'save' => 'Save',
+];
---
+++ lang/es/messages.php
@@ -0,0 +1,13 @@
+<?php
+
+return [
+    'app_name' => 'Gestor de Tareas',
+    'task_list' => 'Lista de Tareas',
+    'create_task' => 'Crear Tarea',
+    'edit_task' => 'Editar Tarea',
+    'task_title' => 'Título',
+    'task_description' => 'Descripción',
+    'task_due_date' => 'Fecha Límite',
+    'task_completed' => 'Completada',
+    'save' => 'Guardar',
+];
